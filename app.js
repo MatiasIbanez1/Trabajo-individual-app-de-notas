048052f19c0e5948b08d83b975bf9ba12006aaed
MatiasIbanez1 <128158660+MatiasIbanez1@users.noreply.github.com>
2023-05-21 10:50:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const path = require('path')
 const express = require("express")
 const app = express()
+const PORT = process.env.PORT || 3000
 app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'views/index.html'))
 })
-app.listen(3000, () => 
-console.log("Servidor Corriendo")
+app.listen(PORT, () => 
+console.log("http://localhost:"+ PORT)
 )
