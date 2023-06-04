61a7569684a214d58cd55cdea0a330e8bad387d8
MatiasIbanez1 <128158660+MatiasIbanez1@users.noreply.github.com>
2023-06-04 11:45:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const path = require('path')
 const express = require("express")
 const app = express()
+
 const PORT = process.env.PORT || 3000
 
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'views/index.html'))
+})
+app.get('/login', function (req, res) {
+    res.sendFile(path.join(__dirname, 'views/login.html'))
 })
 
 app.listen(PORT, () => 
