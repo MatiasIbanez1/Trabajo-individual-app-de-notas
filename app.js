1c36d53d9004f756e2fd58279bfccb8e59c490f2
MatiasIbanez1 <128158660+MatiasIbanez1@users.noreply.github.com>
2023-06-21 14:54:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,18 +4,27 @@ const app = express()
 
 const PORT = process.env.PORT || 3002
 
+const mainRoutes = require ("./routes/mainRoutes")
+const notesRoutes = require ("./routes/notesRoutes")
+
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'views/index.html'))
-})
-app.get('/login', function (req, res) {
-    res.sendFile(path.join(__dirname, 'views/login.html'))
-})
-app.get('', function (req, res) {
-    res.sendFile(path.join(__dirname,"views/mis-notas.html"))
-})
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use("/",mainRoutes)
+app.use("/notes",notesRoutes)
+
+
+// app.get('/', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'views/index.ejs'))
+// })
+// app.get('/login', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'views/login.html'))
+// })
+// app.get('/mis-notas', function (req, res) {
+//     res.sendFile(path.join(__dirname,"views/mis-notas.html"))
+// })
 
 app.listen(PORT, () => 
 console.log("http://localhost:"+ PORT)
-)
