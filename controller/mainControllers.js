9e369cb91c078f2cb68370b965ea43e3e09649c7
MatiasIbanez1 <128158660+MatiasIbanez1@users.noreply.github.com>
2023-06-21 14:30:51 -0300

    Update mainControllers.js

--- controller/mainControllers.js
+++ controller/mainControllers.js
@@ -1,7 +1,17 @@
 const path = require('path')
 const fs = require('fs')
-const productListPath = path.resolve(__dirname,"../database/products.json");
-const productList = JSON.parse(fs.readFileSync(productListPath,'utf-8'));
+//const productListPath = path.resolve(__dirname,"../database/products.json");
+//const productList = JSON.parse(fs.readFileSync(productListPath,'utf-8'));
+
+//app.get('/', function (req, res) {
+    //res.sendFile(path.join(__dirname, 'views/index.ejs'))
+//
+//app.get('/login', function (req, res) {
+    // res.sendFile(path.join(__dirname, 'views/login.html'))
+//
+// app.get('/mis-notas', function (req, res) {
+//     res.sendFile(path.join(__dirname,"views/mis-notas.html"))
+// })
 
 const mainControllers = {
     index:(req,res)=> {
