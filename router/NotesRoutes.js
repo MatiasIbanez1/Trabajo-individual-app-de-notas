fb9cdf108b2ddb19962c60de71e80ed0ebe46b78
MatiasIbanez1 <128158660+MatiasIbanez1@users.noreply.github.com>
2023-07-04 10:01:01 -0300

    Update NotesRoutes.js

--- router/NotesRoutes.js
+++ router/NotesRoutes.js
@@ -3,6 +3,14 @@ const router = express.Router();
 
 const notesControllers = require ("../controllers/notesControllers")
 
+// router.get ("/" , notesControllers.showNotes);
+
+// router.get("/formularioNota",notesControllers.createNote)
+// router.post("/",notesControllers.storeNote)
+
 router.get ("/" , notesControllers.getAllNotes);
+router.get ("/createNotes" , notesControllers.createNotes);
+router.get ("/detailNotes/:id" , notesControllers.getNotebyid);
+router.post ("/", notesControllers.storeNotes);
 
 module.exports = router;
