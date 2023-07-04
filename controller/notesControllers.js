143e4cd7a1c9c355c97488d74e96f2001d3cb53e
MatiasIbanez1 <128158660+MatiasIbanez1@users.noreply.github.com>
2023-07-04 09:57:09 -0300

    Update notesControllers.js

--- controller/notesControllers.js
+++ controller/notesControllers.js
@@ -3,12 +3,20 @@ const path = require('path');
 
 const noteListPath = path.resolve(__dirname, '../database/notes.json');
 const noteList = JSON.parse(fs.readFileSync(noteListPath, 'utf8'));
-
+console.log (noteList)
 const notesControllers = {
     getAllNotes:(req,res)=> {
         res.render('index',{
-            note: noteList
+            noteList: noteList
         })
-}
-    }
-module.exports = notesControllers;
+
+
+},
+
+getNotebyid: (req,res)=> { 
+    let id = req.params.id
+    const filteredNote = noteList.find((note) => {
+        return note.id == id;
+      });
+    res.render ("notes/detailNote",{filteredNote})
+},
